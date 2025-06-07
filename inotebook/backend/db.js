7ca2c04d8f57a2b5050ca085c940d09458a330ad
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-06-07 22:09:45 +0530

    update backend port to 8000

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -4,13 +4,11 @@ const mongoURI = "mongodb://localhost:27017/inotebook";
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB successfully");
+    await mongoose.connect(mongoURI); 
+    console.log("✅ Connected to MongoDB successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Optional: Exit the process if connection fails
   }
 };
 
--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -1,17 +1,18 @@
-const connectToMongo = require('./db');
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
 
 connectToMongo();
-const app = express()
-const port = 3000
-
-app.use(express.json())
+const app = express();
+const port = 8000;
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+  console.log(`iNotebook backend listening at http://localhost:${port}`);
+});
