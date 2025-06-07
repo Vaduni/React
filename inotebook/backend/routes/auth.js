88d94d02439d6e7d666e85baa0196f09b0640ab6
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-06-07 22:08:56 +0530

    Added Hashing password using bcryptjs

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,13 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const { body, validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-//create a user using: POST "/api/auth/createuser". No login required
-router.post('/', (req, res) => {
-	console.log(req.body);
-    const user=User(req.body);
-    user.save();
-	res.send("Hello");
-});
+router.post(
+  "/createuser",
+  [
+    body("name", "Name must be at least 3 characters").isLength({ min: 3 }),
+    body("email", "Please enter a valid email").isEmail(),
+    body("password", "Password must be at least 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //if there are errors in the request body, return a 400 status with the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+//checking whether user with this email already exists
+    try {
+      let existingUser = await User.findOne({ email: req.body.email });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ error: "User with this email already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      console.log("Hashed Password:", hashedPassword);
+//creating a new user
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      const savedUser = await user.save();
+
+      res.status(201).json(savedUser);
+    } catch (error) {
+      console.error("Error saving user:", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+);
 
 module.exports = router;
