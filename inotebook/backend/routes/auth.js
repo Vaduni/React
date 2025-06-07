b77a4a310b0dc8ec88119318c48e3ef57525b5cb
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-06-07 22:26:03 +0530

    installed jwt_secret

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");  
+
+// Replace with env var in production
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
 
 router.post(
   "/createuser",
@@ -10,7 +14,7 @@ router.post(
     body("name", "Name must be at least 3 characters").isLength({ min: 3 }),
     body("email", "Please enter a valid email").isEmail(),
     body("password", "Password must be at least 5 characters").isLength({
-      min: 5,
+      min: 5, 
     }),
   ],
   async (req, res) => {
@@ -19,7 +23,7 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-//checking whether user with this email already exists
+    //checking whether user with this email already exists
     try {
       let existingUser = await User.findOne({ email: req.body.email });
       if (existingUser) {
@@ -31,16 +35,19 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
       console.log("Hashed Password:", hashedPassword);
-//creating a new user
+      //creating a new user
       const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword,
       });
-
-      const savedUser = await user.save();
-
-      res.status(201).json(savedUser);
+      const data={
+        user:{
+            id: user.id
+        }
+      }
+const authToken =jwt.sign(data,JWT_SECRET );
+      res.json({ authToken }); 
     } catch (error) {
       console.error("Error saving user:", error.message);
       res.status(500).json({ error: "Internal Server Error" });
