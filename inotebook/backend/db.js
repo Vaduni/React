144bfbd9cfac77283946cd5b70da946a57462b23
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-06-08 17:15:05 +0530

    Updated port

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = async () => {
   try {
     await mongoose.connect(mongoURI); 
-    console.log("✅ Connected to MongoDB successfully");
+    console.log(" Connected to MongoDB successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Optional: Exit the process if connection fails
