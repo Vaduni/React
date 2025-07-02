81bfbcf91f42dc7aea0a9d79e2c5bea550c4084e
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-07-02 11:12:29 +0530

    deleted extra folders and files for backend

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 connectToMongo();
 const app = express();
