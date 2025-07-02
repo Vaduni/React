86a761b16fdf8fc32af84380fc31b5ddaa8d270d
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-07-02 11:48:21 +0530

    added react router dom

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-  <h1>Hello Vaduni!</h1>
-        
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
