a2967d013003bb98d9de47e3ad1ca89977b29a34
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-08-03 22:00:56 +0530

    changed local database to online database

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI); 
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(" Connected to MongoDB successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1); // Optional: Exit the process if connection fails
+    console.error(" Error connecting to MongoDB:", error.message);
+    process.exit(1); 
   }
 };
 
