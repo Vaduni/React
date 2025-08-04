0465c377889bd403303baf0c13bbbfbb596799e8
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-08-04 10:39:51 +0530

    fixing errors

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -21,6 +21,10 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send(" iNotebook backend is live!");
+});
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
